a3f9ae1e62a491bc9994622150a09b2de0fadc71
zhouqi <17784574@qq.com>
2020-08-20 13:20:14 +0800

    test

--- governance/eureka/src/main/java/com/galaxyt/normae/eureka/EurekaApplication.java
+++ governance/eureka/src/main/java/com/galaxyt/normae/eureka/EurekaApplication.java
@@ -9,9 +9,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaApplication {
-
-
-
     public static void main(String[] args) {
         SpringApplication.run(EurekaApplication.class, args);
         log.info("============================ Eureka * System startup completed ===================================================");
